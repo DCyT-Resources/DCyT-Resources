6646149b1e45d86267f231cf95f30cf94bdb3e9a
rootspyro <root.spyro@gmail.com>
2021-12-18 10:46:34 -0400

    storage shared with exclusive drive

--- db/directories.js
+++ db/directories.js
@@ -11,22 +11,22 @@ const directories = [
 		documents: [
 			{
 				name: 'Cálculo Diferencial - Jorge Saenz',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Calculo%2FCalculo%20diferencial%20-%20Jorge%20Saenz.pdf?alt=media&token=a212c8db-ddc2-43a2-a5f0-4d66145ec7da',
+				path: 'https://drive.google.com/file/d/1s2DntB4E-ZoCTFZwtEnOa7jTy6wuBJa9/view?usp=sharing',
 				icon: 'fas fa-file-pdf',
 			},
 			{
 				name: 'Cálculo Integral - Jorge Saenz',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Calculo%2FCalculo%20Integral%20Jorge%20Saenz.pdf?alt=media&token=0960dcdd-74d1-4d66-a848-6eaf5bf75f6f',
+				path: 'https://drive.google.com/file/d/1mlfStY8NqUf8DEwL5rDYqHzOp02U-4sC/view?usp=sharing',
 				icon: 'fas fa-file-pdf',
 			},
 			{
 				name: 'Cálculo Vectorial - Jorge Saenz',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Calculo%2FCalculo-Vectorial-Jorge-Saenz.pdf?alt=media&token=ed12f8ed-47d9-444d-bf5b-49ad301b1fe8',
+				path: 'https://drive.google.com/file/d/17NBp74HLCm37h1T3ltQz80i7tzQoPa_G/view?usp=sharing',
 				icon: 'fas fa-file-pdf',
 			},
 			{
 				name: 'Cálculo Vectorial - Susan Colley',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Calculo%2FC%C3%A1lculo%20vectorial%20-%20Susan%20Colley.pdf?alt=media&token=acdbf799-fde1-45d7-b5e5-421b3499db78',
+				path: 'https://drive.google.com/file/d/18GkaIGEIC7Kr3_DtTYFzsSKYHI4FtR6o/view?usp=sharing',
 				icon: 'fas fa-file-pdf',
 			},
 			{
@@ -46,12 +46,12 @@ const directories = [
 		documents: [
 			{
 				name: 'Fundamentos de la Matemática - Jorge Saenz',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Estructuras%20Discretas%2FFundamentos%20de%20la%20Matem%C3%A1tica%20-%20Jorge%20Saenz.pdf?alt=media&token=23cbdb68-ecbe-4851-b0f2-8f1c78bd8911',
+				path: 'https://drive.google.com/file/d/1do3QozXTlg0hzEcX0jZbgoe_eqDsykHZ/view?usp=sharing',
 				icon: 'fas fa-file-pdf'
 			},
 			{
 				name: 'Matemáticas Discretas - Johnsonbaugh',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Estructuras%20Discretas%2FMatem%C3%A1ticas%20discretas.pdf?alt=media&token=2e13df2e-a8ce-421a-8bda-d7159d5bfab9',
+				path: 'https://drive.google.com/file/d/1yXKddxsxUM1RJBm0gdqVmt1jS1CvQnF9/view?usp=sharing',
 				icon: 'fas fa-file-pdf'
 			},
 		]
@@ -66,12 +66,12 @@ const directories = [
 		documents: [
 			{
 				name: 'Contabilidad 1 - Francisco Calleja',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Contabilidad%2FContabilidad%201.pdf?alt=media&token=f4394acd-2a75-422c-bb9d-3c1e077be6bf',
+				path: 'https://drive.google.com/file/d/1pHiw0AmkII1omkhF375Ju6mzoH-sq3c6/view?usp=sharing',
 				icon: 'fas fa-file-pdf'
 			},
 			{
 				name: 'Contabilidad - Horngren',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Contabilidad%2FContabilidad%20-%20Horngren.pdf?alt=media&token=7402b998-bab4-4450-97d8-d8e3dd51474a',
+				path: 'https://drive.google.com/file/d/14velb5gDw_S2fiHrBZ4Zlg39l4sk4Zrv/view?usp=sharing',
 				icon: 'fas fa-file-pdf'
 			},
 		]
@@ -96,7 +96,7 @@ const directories = [
 			},
 			{
 				name: 'Clean Code - Robert C Martin',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Porgramacion%2FClean%20Code.pdf?alt=media&token=85f3b56c-69fd-4dbb-9087-66f19aaff05b',
+				path: 'https://enos.itcollege.ee/~jpoial/oop/naited/Clean%20Code.pdf',
 				icon: 'fas fa-file-pdf'
 			},
 			{
@@ -116,12 +116,12 @@ const directories = [
 		documents: [
 			{
 				name: 'Álgebra - Garza',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Algebra%2F%C3%81lgebra%20-%20Garza.pdf?alt=media&token=7bcd04c5-7aa8-4a79-b68b-1142b6c1b543',
+				path: 'https://drive.google.com/file/d/1h3yUhF9yVIEmBcADqgZd2nhzl2CTcdeu/view?usp=sharing',
 				icon: 'fas fa-file-pdf'
 			},
 			{
 				name: 'Álgebra Lineal y sus aplicaciones - Lay',
-				path: 'https://firebasestorage.googleapis.com/v0/b/dcyt-resources.appspot.com/o/Algebra%2F%C3%81lgebra%20lineal%20y%20sus%20aplicaciones%20-%20David%20Lay.pdf?alt=media&token=d5cbbd71-4506-4fdc-8f49-9103a4204ed1',
+				path: 'https://drive.google.com/file/d/1LAHFg32Zkbcd5wLwxrKutp2Q1EVG2BKH/view?usp=sharing',
 				icon: 'fas fa-file-pdf'
 			},
 			{
